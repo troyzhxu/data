95a0117eb235e806dbf39e65c3cceb0656a28047
周旭 <15735559897@qq.com>
2022-03-19 16:24:34 +0800

    优化 GsonDataConvertor

--- data-gson/src/main/java/com/ejlchina/data/gson/GsonDataConvertor.java
+++ data-gson/src/main/java/com/ejlchina/data/gson/GsonDataConvertor.java
@@ -12,9 +12,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class GsonDataConvertor implements DataConvertor {
