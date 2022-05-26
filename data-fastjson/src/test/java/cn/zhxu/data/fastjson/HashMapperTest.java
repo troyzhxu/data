4c27dc55c7b209930b2adbbb82a1e27e6dc02d03
zhouxu <zhou.xu@ejlchina.com>
2022-05-26 18:03:27 +0800

    test

---
+++ data-fastjson/src/test/java/cn/zhxu/data/fastjson/HashMapperTest.java
@@ -0,0 +1,25 @@
+package cn.zhxu.data.fastjson;
+
+import com.ejlchina.data.DataConvertor;
+import com.ejlchina.data.HashMapper;
+import com.ejlchina.data.fastjson.FastjsonDataConvertor;
+import org.junit.Test;
+
+public class HashMapperTest {
+
+    DataConvertor convertor = new FastjsonDataConvertor();
+
+    @Test
+    public void test_01() {
+        HashMapper school = new HashMapper();
+        school.put("name", "清华大学");
+        school.put("address", "北京");
+        HashMapper user = new HashMapper();
+        user.put("name", "Jack");
+        user.put("age", 25);
+        user.put("school", school);
+        String json = convertor.serialize(user);
+        System.out.println(json);
+    }
+
+}
