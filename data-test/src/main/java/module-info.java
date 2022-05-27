413f48e4862c450170e8f1667aecd37e9d13c236
zhouxu <zhou.xu@ejlchina.com>
2022-05-27 22:06:46 +0800

    data-test

--- data-test/src/main/java/module-info.java
+++ data-test/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module data.test {
     requires data.core;
     requires org.junit.jupiter.api;
-    requires com.google.gson;
     opens com.ejlchina.data.test;
     exports com.ejlchina.data.test;
 }
